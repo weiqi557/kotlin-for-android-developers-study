86e7137e8bab57fe7ae65058d9a18303a9e444fb
WeiQI <jelcool557@gmail.com>
2019-07-02 20:02:19 +0800

    write first class

---
+++ app/src/main/java/com/weiqi/android/weatherapp/adapters/ForecastListAdapter.kt
@@ -0,0 +1,20 @@
+package com.weiqi.android.weatherapp.adapters
+
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ForecastListAdapter(val items: List<String>) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(TextView(parent.context))
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = items[position]
+    }
+
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+}
