823ed293dae145f0d08ad36a623f9053934fee3c
WeiQI <jelcool557@gmail.com>
2019-07-02 21:09:10 +0800

    Operatore overloading

--- app/src/main/java/com/weiqi/android/weatherapp/domain/model/DomainClasses.kt
+++ app/src/main/java/com/weiqi/android/weatherapp/domain/model/DomainClasses.kt
@@ -1,14 +1,17 @@
 package com.weiqi.android.weatherapp.domain.model
 
 data class ForecastList(
-    val city:String,
-    val country:String,
-    val dailyForecast:List<Forecast>
-)
+    val city: String,
+    val country: String,
+    val dailyForecast: List<Forecast>
+) {
+    operator fun get(position: Int): Forecast = dailyForecast[position]
+    fun size(): Int = dailyForecast.size
+}
 
 data class Forecast(
-    val date:String,
-    val description:String,
-    val high:Int,
-    val low:Int
+    val date: String,
+    val description: String,
+    val high: Int,
+    val low: Int
 )
